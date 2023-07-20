fa0622aa5640711eb62deed5ef4fbd37dd341858
charlieoneill11 <cponeill00@gmail.com>
2023-07-20 21:34:47 +1000

    refactoring

--- main/inference.py
+++ main/inference.py
@@ -18,72 +18,59 @@ import os
 from utils import format_output
 from main import ContrastiveDecoding
 
-def create_pipeline(local_model_path: str = "/g/data/y89/cn1951/falcon-7b-abstracts-tiny", 
-                    parent_model_path: str = "/g/data/y89/cn1951/falcon-7b"):
-    print("Retrieving model.")
-    fine_tuned_model = AutoModelForCausalLM.from_pretrained(local_model_path, torch_dtype=torch.bfloat16, device_map="auto", trust_remote_code=True)
-    print("Successfully retrieved model. Creating tokenizer and pipeline.")
+class InferencePipeline:
 
-    tokenizer = AutoTokenizer.from_pretrained(parent_model_path)
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=fine_tuned_model,
-        tokenizer=tokenizer,
-        torch_dtype=torch.bfloat16,
-        trust_remote_code=True,
-        device_map="auto",
-    )
+    def __init__(self, local_model_path: str, parent_model_path: str, task: str, output_dir: str = "../results"):
+        self.local_model_path = local_model_path
+        self.parent_model_path = parent_model_path
+        self.output_dir = output_dir
+        self.task = task
+        self.prompt = "### Instruction: Generate a scientific hypothesis about astronomy in the style of an Arxiv paper.\n ### Hypothesis:"
+        self.max_length = 500 if self.task == "abstract" else 150
 
-    print("Successfully created tokenizer and pipeline. Generating hypothesis.")
+    def create_pipeline(self):
+        fine_tuned_model = AutoModelForCausalLM.from_pretrained(self.local_model_path, torch_dtype=torch.bfloat16, 
+                                                                device_map="auto", trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(self.parent_model_path)
+        pipeline = transformers.pipeline(
+            "text-generation",
+            model=fine_tuned_model,
+            tokenizer=tokenizer,
+            torch_dtype=torch.bfloat16,
+            trust_remote_code=True,
+            device_map="auto",
+        )
+        return pipeline, tokenizer
+    
+    def generate_response(self):
+        pipeline, tokenizer = self.create_pipeline()
 
-    return pipeline, tokenizer
+        sequences = pipeline(
+            self.prompt,
+            max_length=self.max_length,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+        )
 
-def generate_abstract():
-    print(f"Cuda availability = {torch.cuda.is_available()}")
-    pipeline, tokenizer = create_pipeline()
+        return format_output(sequences[0]['generated_text'])
+    
+    def generate_synthetic_dataset(self, num_examples: int = 100, save_to_disk: bool = False):
+        dataset = []
+        for i in tqdm(range(num_examples)):
+            dataset.append(self.generate_response())
 
-    sequences = pipeline(
-    "### Instruction: Generate a scientific hypothesis about astronomy in the style of an Arxiv paper.\n ### Hypothesis:",
-        max_length=500,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-    )
+        if save_to_disk:
+            if not os.path.exists(self.output_dir):
+                os.makedirs(self.output_dir)
 
-    return format_output(sequences[0]['generated_text'])
-
-def generate_hypothesis():
-    local_model_path = "/g/data/y89/cn1951/falcon-7b-hypotheses-tiny"
-    parent_model_path = "/g/data/y89/cn1951/falcon-7b"
-
-    print(f"Cuda availability = {torch.cuda.is_available()}")
-    pipeline, tokenizer = create_pipeline(local_model_path=local_model_path, parent_model_path=parent_model_path)
-
-    sequences = pipeline(
-    "### Instruction: Generate a scientific hypothesis about astronomy in the style of an Arxiv paper.\n ### Hypothesis:",
-        max_length=100,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-    )
-
-    return format_output(sequences[0]['generated_text'])
-
-def generate_synthetic_dataset(num_examples: int = 100, task: str = "abstracts", model: str = "falcon-7b"):
-    dataset = []
-    for i in tqdm(range(num_examples)):
-        dataset.append(generate_abstract())
-
-    output_dir = "../results"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    file_path = os.path.join(output_dir, f"{task}-{model}-{num_examples}.txt")
-    with open(file_path, "w") as file:
-        for example in dataset:
-            file.write(example + "\n")
+            file_path = os.path.join(self.output_dir, f"{self.task}-{num_examples}.txt")
+            with open(file_path, "w") as file:
+                for example in dataset:
+                    file.write(example + "\n")
+        
+        return dataset
     
 def contrastive_generation():
     fine_tuned_model_path = "/g/data/y89/cn1951/falcon-7b-abstracts-tiny"
@@ -114,5 +101,7 @@ def contrastive_generation():
     print(tokenizer.decode(outputs[0]))
 
 if __name__ == "__main__":
-    hypothesis = generate_hypothesis()
+    inf_pipe = InferencePipeline(local_model_path="/g/data/y89/cn1951/falcon-7b-abstracts-tiny",
+                                 parent_model_path="/g/data/y89/cn1951/falcon-7b", task="hypothesis")
+    hypothesis = inf_pipe.generate_response()
     print(hypothesis)
